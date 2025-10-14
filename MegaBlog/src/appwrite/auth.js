c9333ec076bbcc4676adf2f59c6ae9397a54e3fd
Abhishek <abisheksavalgi326@gmail.com>
2025-10-14 22:19:22 +0530

    complete auth js with create, login ,logout

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -10,6 +10,42 @@ export class AuthService{
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
+            if(userAccount){
+                // return userAccount;
+                return this.login({email,password});
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try {
+            return await this.account.createEmailSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error ",error);
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions ('current');
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+    
 }
 
 const authService = new AuthService();
