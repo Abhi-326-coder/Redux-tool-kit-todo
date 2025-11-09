91200817ccda4ca110f88f2a2d3af2f6b382760c
Abhishek <abisheksavalgi326@gmail.com>
2025-11-09 22:59:09 +0530

    add Select button and Post Card button

---
+++ MegaBlog/src/components/PostCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Service from "../appwrite/config"
+import {Link} from 'react-router-dom'
+
+export const PostCard = () => {
+  return (
+    <div>PostCard</div>
+  )
+}
