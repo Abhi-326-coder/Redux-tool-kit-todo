0e13f725083295abb0d275ad6e82883c02da505f
Abhishek <abisheksavalgi326@gmail.com>
2025-11-16 10:28:37 +0530

    PostCard and Login component created

--- MegaBlog/src/components/Select.jsx
+++ MegaBlog/src/components/Select.jsx
@@ -11,12 +11,15 @@ const Select = React.forwardRef(function Select({
     
     <div className='w-full'>
         {label && <label htmlFor={id} className=''></label>}
-        <select id = {id} {...props} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}></select>
+        <select id = {id} {...props} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+            
         {options?.map((option)=>(
             <option key={option} value={option}>
                 {option}
             </option>
         ))}
+        
+        </select>
     </div>
   )
 })
